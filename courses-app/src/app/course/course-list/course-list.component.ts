3d5158515e40bb7648fd79328e1880247cfd29e7
dzmitry_platonov <dzmitry_platonov@epam.com>
2020-10-23 08:41:49 +0300

    task 2.1

--- courses-app/src/app/course/course-list/course-list.component.ts
+++ courses-app/src/app/course/course-list/course-list.component.ts
@@ -1,11 +1,6 @@
 import {
-  	AfterContentChecked,
-  	AfterContentInit, AfterViewChecked,
-  	AfterViewInit,
   	Component,
-  	DoCheck,
   	Input,
-  	OnChanges, OnDestroy,
   	OnInit,
 } from '@angular/core';
 import { CourseItem } from '../course-item';
@@ -16,47 +11,18 @@ import { CourseItemsService } from '../course-items.service';
    templateUrl: './course-list.component.html',
    styleUrls: ['./course-list.component.css'],
 })
-export class CourseListComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked,
-	AfterViewInit, AfterViewChecked, OnDestroy {
+export class CourseListComponent implements OnInit {
 
   	public readonly DEFAULT_LIST_SIZE: number = 3;
 
   	public coursesList: CourseItem[];
   	@Input() public searchText: string;
 
-  	constructor(public courseService: CourseItemsService) { }
+  	constructor(private readonly courseService: CourseItemsService) { }
 
   	public ngOnInit(): void {
 		console.log('Courses List on init');
 		this.coursesList = this.courseService.fetch(0, this.DEFAULT_LIST_SIZE);
-  	}
-
-  	public ngDoCheck(): void {
-		console.log('Courses List do check');
-  	}
-
-  	public ngAfterContentInit(): void {
-		console.log('Courses List after content init');
-  	}
-
-  	public ngAfterContentChecked(): void {
-		console.log('Course List after content checked');
-  	}
-
-  	public ngAfterViewInit(): void {
-		console.log('Courses List after view init');
-  	}
-
-  	public ngAfterViewChecked(): void {
-		console.log('Courses List after view check');
-  	}
-
-  	public ngOnDestroy(): void {
-		console.log('Courses List on destroy');
-  	}
-
-  	public ngOnChanges(): void {
-  		console.log('Courses List on changes');
   	}
 
   	public fetchMore(): void {
